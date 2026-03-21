5112599a452b24f49505f1e34649196ff4da8bbb
addkey <addkey0223@gmail.com>
2026-03-21 11:13:24 +0900

    [UI] 커스텀 UI 상호작용 추가

---
+++ Assets/03.Scripts/UI/CharacterCustomizationUI.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterCustomizationUI : MonoBehaviour, IQuantityAdjuster
+{
+    [Header("Name References")]
+    [SerializeField] TMP_InputField _playerNameInput;
+    [SerializeField] TMP_InputField _farmNameInput;
+
+    [Header("Hair Style References")]
+    [SerializeField] List<Sprite> _hairSprites = new List<Sprite>();
+    [SerializeField] TextMeshProUGUI _hairStyleText;
+    [SerializeField] Image _hairPreviewImage;
+
+    string[] _hairStyleNames = { "Culy", "Spikey", "Short", "Mob", "Long", "Bowl" };
+
+    [Header("Color References")]
+    [SerializeField] Slider _colorSlider;
+    [SerializeField] Button _hairColorResetButton;
+
+    [Header("Confirm Button")]
+    [SerializeField] Button _confirmButton;
+
+
+    string _playerName;
+    string _farmName;
+
+    int _currentHairIndex;
+    Color _selectedColor;
+
+
+    void Awake()
+    {
+        _playerNameInput.onValueChanged.AddListener(delegate { UpdateInputText(); });
+        _farmNameInput.onValueChanged.AddListener(delegate { UpdateInputText(); });
+        _colorSlider.onValueChanged.AddListener(OnColorSliderChanged);
+        _hairColorResetButton.onClick.AddListener(OnHairColorResetButtonClicked);
+        _confirmButton.onClick.AddListener(OnConfirmButtonClicked);
+
+        _selectedColor = Color.white;
+        _confirmButton.interactable = false;
+
+
+    }
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    #region Hair
+    void OnColorSliderChanged(float value)
+    {
+        float h = value;
+        _selectedColor = Color.HSVToRGB(h, 1f, 1f);
+        _hairPreviewImage.color = _selectedColor;
+    }
+    void OnHairColorResetButtonClicked()
+    {
+        _selectedColor = Color.white;
+        _hairPreviewImage.color = _selectedColor;
+    }    
+
+    void RefreshHairUI()
+    {
+        _hairStyleText.text = $"¸Ó¸® {_currentHairIndex}";
+        _hairPreviewImage.sprite = _hairSprites[_currentHairIndex];
+    }
+    public void IncreaseQuantity()
+    {
+        if (_currentHairIndex < _hairSprites.Count - 1)
+            _currentHairIndex++;
+        else
+            _currentHairIndex = 0;
+
+        RefreshHairUI();
+    }
+
+    public void DecreaseQuantity()
+    {
+        if (_currentHairIndex > 0)
+            _currentHairIndex--;
+        else
+            _currentHairIndex = _hairSprites.Count - 1;
+
+        RefreshHairUI();
+    }
+    #endregion
+
+    void UpdateInputText()
+    {
+        _playerName = _playerNameInput != null ? _playerNameInput.text : "";
+        _farmName = _farmNameInput != null ? _farmNameInput.text : "";
+        CheckConfirmButton();
+    }
+
+   
+    void CheckConfirmButton()
+    {
+        _confirmButton.interactable = _playerName.Length > 0 && _farmName.Length > 0;
+    }
+    void OnConfirmButtonClicked()
+    {
+
+    }
+}
